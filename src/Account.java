3e284e725086cb915543f0cbeb8873ec350d5993
psreekanth <mail@sreekanth.me>
2015-03-18 23:31:51 -0700

    Good till now

---
+++ src/Account.java
@@ -0,0 +1,42 @@
+public class Account
+{
+	private int accNo, balance;
+	
+	public Account(int a,int b) {
+		accNo=a;
+		balance=b;
+	}
+	public int getBalance(){
+		return balance;
+	}
+	public void depositAmount(int deposit){
+		if(deposit>=0) {
+			balance=balance+deposit;
+			System.out.println("Amount deposited into your account.");
+		}
+		else 
+			System.out.println("Negative amount not accepted");
+		return;
+	}
+	 
+	public void drawAmount(int draw) throws OverdraftException {
+		if(draw>0)
+		{
+			if(draw < balance)
+			{
+				balance=balance-draw;
+			}
+			else {
+				throw new OverdraftException("Funds are not there");
+				// System.out.println("Funds not present");
+			}
+		}
+		else
+			System.out.println("Negative amount not accepted");
+		return;
+	}
+	
+}
+
+
+
